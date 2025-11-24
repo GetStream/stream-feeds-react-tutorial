fdf803a3e00e4985f15584c06a98f695dacf65a8
Zita Szupera <szuperaz@gmail.com>
2025-11-24 09:51:33 +0100

    Activity list

--- src/components/activity/Activity.tsx
+++ src/components/activity/Activity.tsx
@@ -1,3 +1,24 @@
-export const Activity = () => {
-  return '';
+import { ActivityResponse } from '@stream-io/feeds-react-sdk';
+
+export const Activity = ({ activity }: { activity: ActivityResponse }) => {
+  return (
+    <div className="w-full p-4 bg-base-100 card border border-base-300">
+      <div className="w-full flex items-start gap-4">
+        <div className="avatar flex-shrink-0">
+          <div className="w-10 h-10 rounded-full bg-gradient-to-br from-primary to-secondary  flex items-center justify-center text-white text-lg font-semibold">
+            <span>{activity.user?.name?.[0]}</span>
+          </div>
+        </div>
+        <div className="w-full flex flex-col items-start gap-4">
+          <div className="flex flex-row items-center gap-2">
+            <span className="font-semibold text-md">{activity.user.name}</span>
+            <span className="text-sm text-base-content/80">
+              {activity.created_at.toLocaleString()}
+            </span>
+          </div>
+          <p className="w-full">{activity.text}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/components/activity/ActivityList.tsx
+++ src/components/activity/ActivityList.tsx
@@ -1,3 +1,25 @@
+import { useFeedActivities } from '@stream-io/feeds-react-sdk';
+import { Activity } from './Activity';
+
 export const ActivityList = () => {
-  return '';
+  const { activities, loadNextPage, has_next_page } = useFeedActivities();
+
+  return (
+    <div className="w-full flex flex-col items-center justify-start gap-4">
+      {activities?.length === 0 ? (
+        'No posts yet'
+      ) : (
+        <>
+          {activities?.map((activity) => (
+            <Activity activity={activity} key={activity.id} />
+          ))}
+          {has_next_page && (
+            <button className="btn btn-soft btn-primary" onClick={loadNextPage}>
+              Load more
+            </button>
+          )}
+        </>
+      )}
+    </div>
+  );
 };
