f71421d9b282ee5e6ec07a3a5235916d0ccc0a29
Zita Szupera <szuperaz@gmail.com>
2025-11-24 09:57:42 +0100

    Explore page

--- src/pages/Explore.tsx
+++ src/pages/Explore.tsx
@@ -1,3 +1,34 @@
+import {
+  useFeedsClient,
+  useClientConnectedUser,
+  StreamFeed,
+} from '@stream-io/feeds-react-sdk';
+import { useEffect, useMemo } from 'react';
+import { ActivityList } from '../components/activity/ActivityList';
+
 export const Explore = () => {
-  return '';
+  const client = useFeedsClient();
+  const currentUser = useClientConnectedUser();
+  const feed = useMemo(() => {
+    if (!currentUser?.id || !client) {
+      return undefined;
+    }
+    return client.feed('foryou', currentUser.id);
+  }, [client, currentUser?.id]);
+
+  useEffect(() => {
+    if (feed) {
+      feed.getOrCreate({ limit: 10 });
+    }
+  }, [feed]);
+
+  if (!feed) {
+    return null;
+  }
+
+  return (
+    <StreamFeed feed={feed}>
+      <ActivityList />
+    </StreamFeed>
+  );
 };
