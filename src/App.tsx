7dd89613810f6654ead68df596534bb72e12fb3f
Zita Szupera <szuperaz@gmail.com>
2025-11-24 09:47:48 +0100

    Connect to the Stream API

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { StreamFeeds, useCreateFeedsClient } from '@stream-io/feeds-react-sdk';
 import { AppSkeleton } from './AppSkeleton';
+import { API_KEY, CURRENT_USER } from './user';
 
 export default function App() {
-  return <AppSkeleton />;
+  const client = useCreateFeedsClient({
+    apiKey: API_KEY,
+    tokenOrProvider: CURRENT_USER.token,
+    userData: {
+      id: CURRENT_USER.id,
+      name: CURRENT_USER.name,
+    },
+  });
+
+  if (!client) {
+    return null;
+  }
+
+  return (
+    <StreamFeeds client={client}>
+      <AppSkeleton />
+    </StreamFeeds>
+  );
 }
