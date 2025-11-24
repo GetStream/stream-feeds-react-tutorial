e9241c73901272520b5ba44c4ffe5a6be74f4b81
Zita Szupera <szuperaz@gmail.com>
2025-11-24 09:52:46 +0100

    Activity Composer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,20 @@
 import { StreamFeed } from '@stream-io/feeds-react-sdk';
 import { useOwnFeedsContext } from '../own-feeds-context';
+import { ActivityComposer } from '../components/activity/ActivityComposer';
 import { ActivityList } from '../components/activity/ActivityList';
 
 export const Home = () => {
-  const { ownTimeline } = useOwnFeedsContext();
+  const { ownTimeline, ownFeed } = useOwnFeedsContext();
 
-  if (!ownTimeline) {
+  if (!ownTimeline || !ownFeed) {
     return null;
   }
 
   return (
     <div className="w-full flex flex-col items-center justify-start gap-4">
+      <StreamFeed feed={ownFeed}>
+        <ActivityComposer />
+      </StreamFeed>
       <StreamFeed feed={ownTimeline}>
         <ActivityList />
       </StreamFeed>
