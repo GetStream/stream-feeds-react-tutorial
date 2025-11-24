a7fb48138295e5fc73845c843ac5a69008a3bae2
Zita Szupera <szuperaz@gmail.com>
2025-11-24 09:51:33 +0100

    Activity list

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,19 @@
+import { StreamFeed } from '@stream-io/feeds-react-sdk';
+import { useOwnFeedsContext } from '../own-feeds-context';
+import { ActivityList } from '../components/activity/ActivityList';
+
 export const Home = () => {
-  return '';
+  const { ownTimeline } = useOwnFeedsContext();
+
+  if (!ownTimeline) {
+    return null;
+  }
+
+  return (
+    <div className="w-full flex flex-col items-center justify-start gap-4">
+      <StreamFeed feed={ownTimeline}>
+        <ActivityList />
+      </StreamFeed>
+    </div>
+  );
 };
