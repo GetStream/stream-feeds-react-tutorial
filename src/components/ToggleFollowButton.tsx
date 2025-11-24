4b6cc00d264542a00e3db028ca1f36175ec8aa47
Zita Szupera <szuperaz@gmail.com>
2025-11-24 09:55:51 +0100

    Follow and unfollow

--- src/components/ToggleFollowButton.tsx
+++ src/components/ToggleFollowButton.tsx
@@ -1,3 +1,46 @@
-export const ToggleFollowButton = () => {
-  return '';
+import { useCallback, useState } from 'react';
+import { useOwnFeedsContext } from '../own-feeds-context';
+
+export const ToggleFollowButton = ({
+  feed,
+  isFollowing: initialIsFollowing,
+}: {
+  feed: string;
+  isFollowing: boolean;
+}) => {
+  const { ownTimeline } = useOwnFeedsContext();
+  const [isFollowing, setIsFollowing] = useState(initialIsFollowing);
+
+  const follow = useCallback(async () => {
+    await ownTimeline?.follow(feed);
+    setIsFollowing(true);
+    // Reload timelinesto see new activities
+    await ownTimeline?.getOrCreate({ watch: true });
+  }, [feed, ownTimeline]);
+
+  const unfollow = useCallback(async () => {
+    await ownTimeline?.unfollow(feed);
+    setIsFollowing(false);
+    // Reload timeline to remove activities
+    await ownTimeline?.getOrCreate({ watch: true });
+  }, [feed, ownTimeline]);
+
+  const toggleFollow = useCallback(() => {
+    if (isFollowing) {
+      unfollow();
+    } else {
+      follow();
+    }
+  }, [isFollowing, follow, unfollow]);
+
+  return (
+    <button
+      className={`btn btn-soft ${
+        isFollowing ? 'btn-error' : 'btn-primary'
+      } btn-sm`}
+      onClick={toggleFollow}
+    >
+      {isFollowing ? 'Unfollow' : 'Follow'}
+    </button>
+  );
 };
