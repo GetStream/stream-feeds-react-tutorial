f9eee67ed18b0f34cf3cb994f8eb42f6abb7a056
Zita Szupera <szuperaz@gmail.com>
2025-12-02 08:41:35 +0100

    Comments

--- src/components/comments/CommentComposer.tsx
+++ src/components/comments/CommentComposer.tsx
@@ -1,3 +1,38 @@
-export const CommentComposer = () => {
-  return '';
+import { ActivityResponse, useFeedsClient } from '@stream-io/feeds-react-sdk';
+import { useState, useCallback } from 'react';
+
+export const CommentComposer = ({
+  activity,
+}: {
+  activity: ActivityResponse;
+}) => {
+  const client = useFeedsClient();
+  const [commentDraft, setCommentDraft] = useState('');
+
+  const addComment = useCallback(async () => {
+    await client?.addComment({
+      object_id: activity.id,
+      object_type: 'activity',
+      comment: commentDraft,
+    });
+    setCommentDraft('');
+  }, [client, activity.id, commentDraft]);
+
+  return (
+    <div className="w-full flex flex-row gap-2">
+      <input
+        className="input w-full"
+        placeholder="Post your reply"
+        value={commentDraft}
+        onChange={(e) => setCommentDraft(e.target.value)}
+      />
+      <button
+        className="btn btn-primary"
+        onClick={addComment}
+        disabled={!commentDraft.trim()}
+      >
+        Reply
+      </button>
+    </div>
+  );
 };
--- src/components/comments/CommentList.tsx
+++ src/components/comments/CommentList.tsx
@@ -1,3 +1,38 @@
-export const CommentList = () => {
-  return '';
+import {
+  ActivityResponse,
+  useActivityComments,
+} from '@stream-io/feeds-react-sdk';
+import { useEffect } from 'react';
+
+export const CommentList = ({ activity }: { activity: ActivityResponse }) => {
+  const {
+    comments = [],
+    loadNextPage,
+    has_next_page,
+  } = useActivityComments({ activity });
+
+  // Load initial comments
+  useEffect(() => {
+    if (comments.length === 0 && activity.comment_count > 0) {
+      void loadNextPage({ limit: 5, sort: 'best' });
+    }
+  }, [loadNextPage, comments.length, activity.comment_count]);
+  return (
+    <>
+      {comments.map((comment) => (
+        <div className="flex flex-row items-center gap-2" key={comment.id}>
+          <span className="font-semibold">{comment.user.name}:</span>
+          <span>{comment.text}</span>
+        </div>
+      ))}
+      {activity.comment_count > 0 && has_next_page && (
+        <button
+          className="btn btn-soft btn-primary"
+          onClick={() => loadNextPage()}
+        >
+          Load more comments
+        </button>
+      )}
+    </>
+  );
 };
